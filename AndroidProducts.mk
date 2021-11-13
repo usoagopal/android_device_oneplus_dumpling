2cdd583031a718bec4542eda7951a4319107c2d2
usoagopal <94131230+usoagopal@users.noreply.github.com>
2021-11-13 18:34:00 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/nad_dumpling.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dumpling-user \
-    lineage_dumpling-userdebug \
-    lineage_dumpling-eng
+    nad_dumpling-user \
+    nad_dumpling-userdebug \
+    nad_dumpling-eng
